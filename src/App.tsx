908bfa0e28bff5511afb805a4794bca0bc64dd0b
Saaniya Shafqat <saaniya1902@gmail.com>
2025-08-21 16:43:59 +0100

    Create header and footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import {useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import {
   Main,
+  Navigation,
+  Footer,
 } from "./components";
 import './index.scss';
 
 function App() {
+    const [mode, setMode] = useState<string>('dark');
+
+    const handleModeChange = () => {
+        if (mode === 'dark') {
+            setMode('light');
+        } else {
+            setMode('dark');
+        }
+    }
+
     useEffect(() => {
         window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
       }, []);
 
     return (
-    <div className={`main-container`}>
+    <div className={`main-container ${mode === 'dark' ? 'dark-mode' : 'light-mode'}`}>
+        <Navigation parentToChild={{mode}} modeChange={handleModeChange}/>
             <Main/>
+        <Footer />
     </div>
     );
 }

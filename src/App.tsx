272c8f9f35ed4a9ca21104c150ca907a76fa3375
Saaniya Shafqat <saaniya1902@gmail.com>
2025-08-21 16:22:18 +0100

    Configure main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from "react";
+import {
+  Main,
+} from "./components";
+import './index.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    useEffect(() => {
+        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+      }, []);
+
+    return (
+    <div className={`main-container`}>
+            <Main/>
     </div>
-  );
+    );
 }
 
 export default App;

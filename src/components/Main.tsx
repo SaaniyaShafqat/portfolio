c72ffa1ac9c7136d07175eabed9b81873bb32405
Saaniya Shafqat <saaniya1902@gmail.com>
2025-08-21 16:16:11 +0100

    Install and configure sass

---
+++ src/components/Main.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../assets/styles/Main.scss';
+
+function Main() {
+
+  return (
+    <div className="container">
+      <div className="about-section">
+        <div className="image-wrapper">
+          <img src="https://media.licdn.com/dms/image/v2/D4E03AQH6Aov3FrmEgg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1732131347808?e=1758758400&v=beta&t=FsqTmd92OVgaZRu36Gw2iTL_ySNnLXPaLLmhiim6DF4" alt="Avatar" />
+        </div>
+        <div className="content">
+          <div className="social_icons">
+            <a href="https://github.com/SaaniyaShafqat" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/saaniya-s/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+          <h1>Saaniya Shafqat</h1>
+          <p>Web Developer</p>
+
+          <div className="mobile_social_icons">
+            <a href="https://github.com/SaaniyaShafqat" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/saaniya-s/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
